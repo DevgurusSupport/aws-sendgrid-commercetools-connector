16771ba20c274ae7e41ef51a696831750a6f5979
Daniel Martínez <damarna85@gmail.com>
2018-07-31 12:54:36 +0200

    add order created case and update readme file

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,8 @@ export const buildMessage = msg => {
   switch (msg.type) {
     case 'CustomerCreated':
       return generateCustomerCreatedMessage(msg);
+    case 'OrderCreated':
+      return generateOrderCreatedMessage(msg);
     default:
       console.warn(`Ignoring message: ${JSON.stringify(msg)}`);
       return null;
@@ -19,5 +21,19 @@ const generateCustomerCreatedMessage = msg => ({
   substitutions: {
     email: msg.customer.email,
     firstName: msg.customer.firstName,
+  },
+});
+
+const generateOrderCreatedMessage = msg => ({
+  to: msg.order.customerEmail,
+  from: msg.fromEmail,
+  subject: `Your order ${msg.order.orderNumber}`,
+  html: 'Order successfully created!',
+  // in case you use a template, set the id here
+  // templateId: '',
+  substitutions: {
+    orderNumber: msg.order.orderNumber,
+    customerEmail: msg.order.customerEmail,
+    totalPrice: msg.order.totalPrice,
   },
 });

3c548650571aa6acc708cc0344af44705b68b3e3
Daniel Martínez <damarna85@gmail.com>
2018-08-02 11:05:33 +0200

    se client as object

--- clients/commercetools/index.js
+++ clients/commercetools/index.js
@@ -3,24 +3,22 @@ import { createClient } from '@commercetools/sdk-client';
 import { createHttpMiddleware } from '@commercetools/sdk-middleware-http';
 import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth';
 
-const client = () =>
-  createClient({
-    middlewares: [
-      createAuthMiddlewareForClientCredentialsFlow({
-        host:
-          process.env.COMMERCETOOLS_AUTH_HOST ||
-          'https://auth.commercetools.co',
-        projectKey: process.env.COMMERCETOOLS_PROJECT_KEY,
-        credentials: {
-          clientId: process.env.COMMERCETOOLS_CLIENT_ID,
-          clientSecret: process.env.COMMERCETOOLS_CLIENT_SECRET,
-        },
-      }),
-      createHttpMiddleware({
-        host:
-          process.env.COMMERCETOOLS_API_HOST || 'https://api.commercetools.co',
-      }),
-    ],
-  });
+const client = createClient({
+  middlewares: [
+    createAuthMiddlewareForClientCredentialsFlow({
+      host:
+        process.env.COMMERCETOOLS_AUTH_HOST || 'https://auth.commercetools.co',
+      projectKey: process.env.COMMERCETOOLS_PROJECT_KEY,
+      credentials: {
+        clientId: process.env.COMMERCETOOLS_CLIENT_ID,
+        clientSecret: process.env.COMMERCETOOLS_CLIENT_SECRET,
+      },
+    }),
+    createHttpMiddleware({
+      host:
+        process.env.COMMERCETOOLS_API_HOST || 'https://api.commercetools.co',
+    }),
+  ],
+});
 
 export { client };
